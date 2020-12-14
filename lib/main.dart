975d654e246917b054085e7edec2c6447eecb816
thedrivingforce <thedrivingforc@users.noreply.github.com>
2020-12-14 19:55:15 +0530

    decorations

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,38 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text("Hi Flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          // color: Colors.red,
+          width: 100,
+          height: 100,
+          // clipBehavior: Clip.antiAlias,
+          alignment: Alignment.topLeft,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              // shape: BoxShape.circle,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [
+                Colors.pink,
+                Colors.yellow,
+              ]),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey[900],
+                  blurRadius: 10,
+                )
+              ]),
+          child: Text(
+            "I am a box ",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
