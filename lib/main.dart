a87aff2f0bc61f280b4a9b476e7b183562822663
thedrivingforce <thedrivingforc@users.noreply.github.com>
2020-12-15 16:49:54 +0530

    ch3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -9,50 +9,97 @@ void main() {
   ));
 }
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+// !Changed to stateful widget
+class _HomepageState extends State<Homepage> {
+  var myText = "Change My Text";
+  TextEditingController _nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        height: 100,
-        // width: 500,
-        color: Colors.teal[100],
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: [
+                  Image.asset(
+                    "assets/bg.jpg",
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    myText,
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                  ),
+                  TextField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter some text",
+                        labelText: "Name"),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
           children: [
-            Container(
-              padding: const EdgeInsets.all(8),
-              // color: Colors.red,
-              width: 100,
-              height: 100,
-              color: Colors.red,
-              // clipBehavior: Clip.antiAlias,
-              alignment: Alignment.topLeft,
+            UserAccountsDrawerHeader(
+                accountName: Text("Mritunjay"),
+                accountEmail: Text("mritrunjay@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1510552776732-03e61cf4b144?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Ym95fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
+                )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Mritunjay"),
+              subtitle: Text("App Developer"),
+              trailing: Icon(Icons.edit),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              // color: Colors.red,
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-              // clipBehavior: Clip.antiAlias,
-              alignment: Alignment.topLeft,
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Mail At"),
+              subtitle: Text("mritrunjay@gmail.com"),
+              trailing: Icon(Icons.edit),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              // color: Colors.red,
-              width: 100,
-              height: 100,
-              color: Colors.green,
-              // clipBehavior: Clip.antiAlias,
-              alignment: Alignment.topLeft,
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text("Ritu"),
+              subtitle: Text("App Reviewer"),
+              trailing: Icon(Icons.edit),
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {}); // Direct change of text on press
+        },
+        child: Icon(Icons.favorite),
       ),
     );
   }
